2b9a831bde24abfd574375cb16c4d37c5944bf19
Asiful Alam Fahim <asiful.alam@northsouth.edu>
2019-02-23 19:31:25 +0700

    Sign Up Page
    This is a sign up page which contains Password, Mail and User Name.

---
+++ Sign Up.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+</head>
+</body>
+<form method="post" action="sign_up.php" enctype="multipart/form-data">
+<table align="center" border="1"style="width:70%; margin-top:40px;">
+<h1 align="center"> SIGN UP</h1>
+
+<tr>
+<th>User Name</th>
+
+<td><input type="text" name="name" placeholder="enter User Name" required></td>
+
+</tr>
+
+
+<tr>
+<th>Gmail</th>
+
+<td><input type="text" name="gmail" placeholder="enter email" required></td>
+
+</tr>
+
+
+
+<tr>
+<th>Password</th>
+
+<td><input type="twxt" name="pass" placeholder="Enter a password" required></td>
+
+</tr>
+
+
+
+<tr>
+
+<td colspan="2" align="center"><input type="submit" name="submit"  value="Submit"></td>
+
+</tr>
+
+
+
+
+
+</table>
+</form>
+</body>
+</html>
