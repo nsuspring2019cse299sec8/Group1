5db3cfb47d874c7f0111c535815b36b786396633
mitu <47188195+MITU-RANI-DAS@users.noreply.github.com>
2019-03-01 22:35:34 +0600

    Update mms.sql
    update database and add to insert product table

--- mms.sql
+++ mms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 22, 2019 at 05:59 AM
+-- Generation Time: Feb 25, 2019 at 07:03 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.1
 
@@ -43,6 +43,29 @@ INSERT INTO `admin` (`ID`, `USERNAME`, `GMAIL`, `PASSWORD`) VALUES
 (2, 'admin', 'admin1234@gmail.com', '1234'),
 (3, 'Anik', 'anikulislam123@gmail.com', '78910');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_insert_table`
+--
+
+CREATE TABLE `product_insert_table` (
+  `ID` int(100) NOT NULL,
+  `PRODUCT_NAME` varchar(100) NOT NULL,
+  `PRODUCT_ID` int(50) NOT NULL,
+  `PRODUCT_PRICE` varchar(50) NOT NULL,
+  `PRODUCT_UPDATE_PRICE` varchar(50) NOT NULL,
+  `IMAGE` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `product_insert_table`
+--
+
+INSERT INTO `product_insert_table` (`ID`, `PRODUCT_NAME`, `PRODUCT_ID`, `PRODUCT_PRICE`, `PRODUCT_UPDATE_PRICE`, `IMAGE`) VALUES
+(7, 'i-phone-6', 258963, '55000', '50000', 'i-phone6.PNG'),
+(8, 'Nokia 10', 258741, '13000', '13000', 'nokia.PNG');
+
 --
 -- Indexes for dumped tables
 --
@@ -54,6 +77,12 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`ID`);
 
 --
+-- Indexes for table `product_insert_table`
+--
+ALTER TABLE `product_insert_table`
+  ADD PRIMARY KEY (`ID`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -62,6 +91,12 @@ ALTER TABLE `admin`
 --
 ALTER TABLE `admin`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `product_insert_table`
+--
+ALTER TABLE `product_insert_table`
+  MODIFY `ID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
