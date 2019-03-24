b9a83fb92dcacc0f4a96c9531730b50796d2b789
Asiful Alam Fahim <asiful.alam@northsouth.edu>
2019-03-24 13:11:40 +0600

    Team member information
    In this part i've added our team information including mail address and pictures.

---
+++ team design.php
@@ -0,0 +1,44 @@
+   <section id="testimonial">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 col-sm-offset-2">
+
+                    <div id="carousel-testimonial" class="carousel slide text-center" data-ride="carousel">                      
+                        <div class="carousel-inner" role="listbox">
+                            
+							<div class="item active">
+                                <p><img class="img-circle img-thumbnail" src="images/testimonial/anik.jpeg" alt=""></p>
+                                <h4>Md Anikul Islam</h4>
+                                <small>anikul.islam01@northsouth.edu</small>                              
+                            </div>
+							
+                            <div class="item">
+                                <p><img class="img-circle img-thumbnail" src="images/testimonial/mitu (2).jpg" alt=""></p>
+                                <h4>Mitu Rani Das</h4>
+                                <small>mitu.das@northsouth.edu</small>                               
+                            </div>
+							
+							<div class="item">
+                                <p><img class="img-circle img-thumbnail" src="images/testimonial/fa.jpg" alt=""></p>
+                                <h4>Asiful Islam Fahim</h4>
+                                <small>asiful.alam@northsouth.edu</small>                               
+                            </div>
+                        </div>
+
+                        
+                        <div class="btns">
+                                <a class="btn btn-primary btn-sm" href="#carousel-testimonial" role="button" data-slide="prev">
+                                <span class="fa fa-angle-left" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                                </a>
+								
+                                <a class="btn btn-primary btn-sm" href="#carousel-testimonial" role="button" data-slide="next">
+                                <span class="fa fa-angle-right" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                                </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
